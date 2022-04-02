a372d6a5abe2d97bc760f112b0bf8f34e2bfd89e
Manuel Lorenzo <manulorenzop@gmail.com>
2022-04-02 17:45:46 +0200

    try to add SVG icon

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [
     new FaviconsWebpackPlugin({
-      logo: './icon.png',
+      logo: './icon.svg',
       manifest: './config/manifest.json',
     }),
     new HtmlWebpackPlugin({
